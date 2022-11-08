8c968a0f28e02d077c65c015dc4c967d582bd345
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-08 16:55:11 -0500

    router.post to create a new category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -38,6 +38,12 @@ router.get('/:id', async (req, res) => {
 // POST, PUT, DELETE declarations 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
