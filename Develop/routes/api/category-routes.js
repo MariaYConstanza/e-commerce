9625c5a62d598a1b5a5adceb084e5d7df6fa5b5a
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-08 16:45:35 -0500

    router.get to find a single category from its id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,9 +14,25 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    
+    // return to this function to checkproof
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Category, through: Product, as: 'category_products' }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category was found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
